8d8621eed9acbb6116c304d5028ec4cfee0bb49f
˜Leonardo <leonardo.pbl@hotmail.com>
2026-02-26 22:39:03 -0300

    Revert "chore: unify repository structure and update docs"
    This reverts commit f56c36be8a66754a81b9a287f024755b3a012a9a.
    
    Signed-off-by: ˜Leonardo <leonardo.pbl@hotmail.com>

--- api/nova_control/lib/nova_control/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule NovaControl.Mailer do
-  use Swoosh.Mailer, otp_app: :nova_control
-end
--- api/nova_control/lib/nova_control/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule NovaControl.Repo do
-  use Ecto.Repo,
-    otp_app: :nova_control,
-    adapter: Ecto.Adapters.Postgres
-end
